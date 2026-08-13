709132e08a47b711311fb44d65bec184b7a92e1e
Samarth <2025.samarths@isu.ac.in>
2026-08-13 12:03:38 +0530

    hcfmff

--- Assignment/HospitalManagementAPI/hm-crud/config/db.js
+++ Assignment/HospitalManagementAPI/hm-crud/config/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/hospital_management";
 
 mongoose.connect(MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => console.log("Connected to MongoDB database:", mongoose.connection.name))
   .catch((err) => console.log("MongoDB connection error:", err));
 
 module.exports = mongoose;
