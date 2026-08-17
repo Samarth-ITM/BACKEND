65be31142c680ad17fe4e3a5190dee027f2bea66
Samarth <2025.samarths@isu.ac.in>
2026-08-17 10:26:26 +0530

    com1

--- Assignment/HospitalManagementAPI/hm-crud/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const passport = require("passport");
-const router = express.Router();
-const { register, login } = require("../controllers/authController");
-
-router.post("/register", register);
-router.post("/login", passport.authenticate("local"), login);
-
-module.exports = router;
